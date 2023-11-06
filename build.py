29fd0649a28278bd1843fc741280c891f50e6eca
William Cook <wjjcook11@gmail.com>
2023-11-05 20:44:27 -0800

    Updated build.py and created exe

--- build.py
+++ build.py
@@ -5,12 +5,15 @@ def main():
    
     # Example: Create an executable using PyInstaller
     pyinstaller_command = [
-        "pyinstaller",    # Replace with the path to your PyInstaller executable, if necessary
+        "pyinstaller",
+        "--noconfirm",
+        "--onedir",      # Create a single executable file
+        "--windowed",
+        "--add-data", "graphics;graphics/", "--add-data", "code;code/", "--add-data", "map.json;.",
         "code/game.py",  # Replace with the entry script of your project
-        "--onefile",      # Create a single executable file
         "--name", "Dungeon Knightmare",  # Name of the executable file (you can customize this)
     ]
-    
+
     try:
         subprocess.run(pyinstaller_command, check=True)
         print("Executable created successfully")
